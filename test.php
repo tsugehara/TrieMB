691ba9b171394581943b27d136aa69841ea51b9e
Tsugehara Toyokazu <tsugehara@gmail.com>
2013-07-03 22:37:55 -0400

    update test program

--- test.php
+++ test.php
@@ -7,8 +7,17 @@ $tree->add('a');
 $tree->add('ai ga tomaranai');
 $tree->add('aiueo');
 $tree->add('akasaatana');
+$tree->add('kakikukeko');
+$tree->add('あかさたな');
+$tree->add('あいうえお');
 
-$tests = array('a', 'ai', 'aiu');
+$tests = array('a', 'ai', 'aiu', 'あ', 'あい');
+
+echo '<div>';
+echo '<h2>'.htmlspecialchars('All').'</h2>';
+print_r($tree->getAll());
+echo '</div>';
+
 foreach ($tests as $test) {
     echo '<div>';
     echo '<h2>'.htmlspecialchars($test).'</h2>';
